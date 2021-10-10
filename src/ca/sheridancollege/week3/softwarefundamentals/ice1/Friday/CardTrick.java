b1d7ba7b24fd94800c2a4d4e1b52c4f956d6df9c
htian22 <lihaoti@sheridancollege.ca>
2021-10-09 21:38:54 -0400

    added Card obj in CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,14 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
- * @author Ronak
+ * Edited by Haotian Li
+ * 991609029
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       CardTrick Card = new CardTrick();
+       System.out.println(Card);
     }
     
 }
